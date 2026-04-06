cededd301d814794ceef9ba22450b3831045474a
leejuyeon7852 <leejuyeon7852@gmail.com>
2026-04-06 15:14:31 +0900

    feat: fragment_profile.xml 화면 구성

---
+++ app/src/main/java/com/umc/workbook/week2/ui/profile/adapter/FollowingAdapter.kt
@@ -0,0 +1,12 @@
+package com.umc.workbook.week2.ui.profile.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.workbook.week2.databinding.ItemFollowingBinding
+
+class FollowingAdapter {
+}
+
+class FollowingViewHolder(val binding: ItemFollowingBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+        }
