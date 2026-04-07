57b7fd46146c8517de9e98f6c4ac5c43d243c18a
leejuyeon7852 <leejuyeon7852@gmail.com>
2026-04-07 14:04:07 +0900

    feat: 프로필 API 연결 & 구현

--- app/src/main/java/com/umc/workbook/week2/ui/profile/adapter/FollowingAdapter.kt
+++ app/src/main/java/com/umc/workbook/week2/ui/profile/adapter/FollowingAdapter.kt
@@ -1,12 +1,44 @@
 package com.umc.workbook.week2.ui.profile.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.umc.workbook.week2.data.UserData
 import com.umc.workbook.week2.databinding.ItemFollowingBinding
 
-class FollowingAdapter {
+class FollowingAdapter(
+        private val itemList: MutableList<UserData>,
+) : RecyclerView.Adapter<FollowingViewHolder>(){
+        override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+        ): FollowingViewHolder {
+                val binding = ItemFollowingBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                )
+                return FollowingViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: FollowingViewHolder, position: Int) {
+                holder.bind(itemList[position])
+        }
+
+        override fun getItemCount() = itemList.size
+
+        fun updateList(newList: List<UserData>) {
+                itemList.clear()
+                itemList.addAll(newList)
+                notifyDataSetChanged()
+        }
+
 }
 
 class FollowingViewHolder(val binding: ItemFollowingBinding):
         RecyclerView.ViewHolder(binding.root){
-
+                fun bind(item: UserData){
+                        Glide.with(binding.root)
+                                .load(item.avatar)
+                                .into(binding.ivFollowingProfile)
+                }
         }
